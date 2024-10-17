5a2e58075f4eec2939d5834270337e687b9495f7
Luiz Chaves <lucachaves@gmail.com>
2024-10-17 06:14:22 -0300

    classroom: ecma syntax

---
+++ classroom/ecma/data-type.js
@@ -0,0 +1,41 @@
+// undefined
+console.log(undefined);
+
+// null
+console.log(null);
+
+// boolean
+console.log(true);
+console.log(false);
+
+// number
+console.log(15);
+console.log(0b1111);
+console.log(0o17);
+console.log(0xf);
+console.log(3.14);
+console.log(314e-2);
+
+// IEEE 754
+// https://0.30000000000000004.com/
+
+// NaN
+console.log(1 + undefined);
+
+// Infinity
+console.log(1 / 0);
+
+// string
+console.log('Hello');
+console.log("Hello");
+console.log(`Hello`);
+
+// array
+console.log(['Alice', 'alice@email.com', 18]);
+
+// json
+console.log({
+  name: 'Alice',
+  email: 'alice@email.com',
+  age: 18
+})
---
+++ classroom/ecma/variable.js
@@ -0,0 +1,11 @@
+// const
+
+// 'const' declarations must be initialized.
+
+// TypeError: Assignment to constant variable.
+
+// Cannot redeclare block-scoped variable 'number'.
+
+// let
+
+// var
