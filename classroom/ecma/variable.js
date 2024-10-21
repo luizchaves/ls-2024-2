d5b81f275a830155d71d594f57ca5ad097405ee8
Luiz Chaves <lucachaves@gmail.com>
2024-10-21 16:08:08 -0300

    classroom: syntax js - variable, expressions, statements

--- classroom/ecma/variable.js
+++ classroom/ecma/variable.js
@@ -1,11 +1,37 @@
 // const
+const number = 10;
 
 // 'const' declarations must be initialized.
+// const value;
 
 // TypeError: Assignment to constant variable.
+// number = 100;
 
 // Cannot redeclare block-scoped variable 'number'.
+// const number = 100;
+
+function hello() {
+  const number = 100;
+
+  console.log(number);
+}
 
 // let
+let value = 10;
+
+value = 100;
+
+console.log(value);
+
+let personName;
+
+personName = 'Alice';
+
+// let number;
 
 // var
+var name = 'Alice';
+
+var name = 10;
+
+// const vs let vs var
