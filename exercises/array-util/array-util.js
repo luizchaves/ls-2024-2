c3aa7e6b9726ca7b68e6d007559cd279fa7b96b3
Luiz Chaves <lucachaves@gmail.com>
2024-11-04 15:39:24 -0300

    exercises: array-util

---
+++ exercises/array-util/array-util.js
@@ -0,0 +1,23 @@
+export function min(array) {
+  return Math.min(...array);
+}
+
+export function max(array) {
+  return Math.max(...array);
+}
+
+export function range(length, last, step) {
+  // TODO
+}
+
+export function zip(...arrays) {
+  // TODO
+}
+
+export function uniq(array) {
+  // TODO
+}
+
+export function sortNum(array) {
+  // TODO
+}
