fd6034d7936a73367aa722a0c1e13cd814d17f19
Luiz Chaves <lucachaves@gmail.com>
2025-02-24 15:20:05 -0300

    classroom: next with supabase

--- projects/invest-app-next/src/app/page.js
+++ projects/invest-app-next/src/app/page.js
@@ -1,35 +1,12 @@
 import InvestmentCard from '@/components/InvestmentCard';
+import Storage from '@/services/storage';
 
-export default function Home() {
-  const investments = [
-    {
-      id: 'b9f2414d-b8dd-484d-8179-83383d10a3fd',
-      name: 'Tesouro Selic 2031',
-      value: 10050,
-      origin: 'Tesouro Nacional',
-      category: 'Pós',
-      created_at: '2023-08-22T00:00:00-03:00',
-      interest: '100% Selic',
-    },
-    {
-      id: '362fcbac-66f4-43d9-990f-547d60551b94',
-      name: 'Tesouro Selic 2031',
-      value: 20050,
-      origin: 'Tesouro Nacional',
-      category: 'Pós',
-      created_at: '2023-08-22T00:00:00-03:00',
-      interest: '100% Selic',
-    },
-    {
-      id: 'c3944887-db03-4eba-968e-2c8391cebfdc',
-      name: 'Tesouro Selic 2031',
-      value: 50050,
-      origin: 'Tesouro Nacional',
-      category: 'Pós',
-      created_at: '2023-08-22T00:00:00-03:00',
-      interest: '100% Selic',
-    },
-  ];
+async function getData() {
+  return await Storage.read('investments');
+}
+
+export default async function Home() {
+  const investments = await getData();
 
   return (
     <>
